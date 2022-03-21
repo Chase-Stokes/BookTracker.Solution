d1330bd505bd9b43fd1fef179a6c513c03687b1e
Chase Stokes <chasehunterstokes@gmail.com>
2022-03-21 11:01:20 -0700

    update models for author/book/jointable
    Co-authored-by: DrewHendersonGitHub <DrewHendersonGitHub@users.noreply.github.com>

---
+++ Library/Models/Book.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System;
+
+namespace Library.Models
+{
+  public class Book
+  {
+    public Book()
+    {
+      this.JoinEntities = new HashSet<AuthorBook>();
+    }
+    public int BookId { get; set; }
+    public string Title { get; set; }
+    public string Series { get; set; }
+    public string Genre { get; set; }
+    public DateTime Published { get; set; }
+
+    public virtual ICollection<AuthorBook> JoinEntities { get; }
+  }
+}
